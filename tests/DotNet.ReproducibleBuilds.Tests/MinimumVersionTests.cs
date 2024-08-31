40a699868eb77f109295ce330854ac4bd0ee8538
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-08-30 18:21:16 -0700

    Add unit tests

---
+++ tests/DotNet.ReproducibleBuilds.Tests/MinimumVersionTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Microsoft.Build.Utilities.ProjectCreation;
+
+namespace DotNet.ReproducibleBuilds.Tests;
+
+public class MinimumVersionTests : TestBase
+{
+    [Theory]
+    [InlineData("17.7.0", false)]
+    [InlineData("17.8.0", true)]
+    [InlineData("17.9.0", true)]
+    public void BelowMinimumVersionEmitsWarning(string msbuildVersion, bool success)
+    {
+        Dictionary<string, string> globalProperties = new()
+        {
+            ["_ReproducibleBuildsMSBuildVersion"] = msbuildVersion
+        };
+
+        ProjectCreator.Templates
+            .ReproducibleBuildProject(GetRandomFile(".csproj"))
+            .TryBuild(restore: false, target: "_ReproducibleBuildsMSBuildVersionCheck", globalProperties, out bool result, out BuildOutput output);
+
+        result.Should().BeTrue();
+        output.Warnings.Should().HaveCount(success ? 0 : 1);
+    }
+}
