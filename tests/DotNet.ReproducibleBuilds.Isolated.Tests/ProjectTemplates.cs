202ad55b8df8d96a1a3e3cfdd0f4dd2deeb29b6c
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-09-15 11:48:22 -0700

    Add suppression tests

---
+++ tests/DotNet.ReproducibleBuilds.Isolated.Tests/ProjectTemplates.cs
@@ -0,0 +1,24 @@
+﻿using DotNet.ReproducibleBuilds.Tests.Shared;
+
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Utilities.ProjectCreation;
+
+namespace DotNet.ReproducibleBuilds.Isolated.Tests;
+
+internal static class ProjectTemplates
+{
+    private static readonly string ThisAssemblyDirectory = Path.GetDirectoryName(typeof(ProjectTemplates).Assembly.Location)!;
+
+    public static ProjectCreator ReproducibleBuildsIsolatedProject(this ProjectCreatorTemplates templates, FileInfo project, Action<ProjectCreator>? configureProject = null)
+    {
+        DirectoryInfo directory = project.Directory ?? throw new ArgumentException("Project's path does not appear to have a parent.", nameof(project));
+
+        ProjectCollection projectCollection = new(); // Create a new collection for each project to ensure environment variables aren't shared between tests
+
+        return ProjectCreator
+            .Create(path: project.FullName, sdk: ProjectCreatorConstants.SdkCsprojDefaultSdk, projectCollection: projectCollection)
+            .Import(Path.Combine(ThisAssemblyDirectory, "Sdk.props"))
+            .CustomAction(configureProject)
+            .Import(Path.Combine(ThisAssemblyDirectory, "Sdk.targets"));
+    }
+}
---
+++ tests/DotNet.ReproducibleBuilds.Isolated.Tests/TargetFrameworkRootPathTests.cs
@@ -0,0 +1,34 @@
+﻿using DotNet.ReproducibleBuilds.Tests.Shared;
+
+using FluentAssertions;
+using Microsoft.Build.Utilities.ProjectCreation;
+
+namespace DotNet.ReproducibleBuilds.Isolated.Tests;
+
+public class TargetFrameworkRootPathTests : TestBase
+{
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public void WarningCanBeSuppressed(bool suppress)
+    {
+        ProjectCreator.Templates
+            .ReproducibleBuildsIsolatedProject(GetRandomFile(".csproj"), configureProject: project =>
+            {
+                project.Property("TargetFramework", "net472");
+
+                if (suppress)
+                {
+                    project.Property("NoWarn", "RPB0002");
+                }
+            })
+            // Run the target directly and _not_ as part of Build because we want to test the warning path and not allow the SDK's
+            // automatic references to run.
+            .TryBuild(restore: false, target: "CheckTargetFrameworkRootPath", out bool result, out BuildOutput output);
+
+        int expected = suppress ? 0 : 1;
+
+        result.Should().BeTrue();
+        output.Warnings.Should().HaveCount(expected);
+    }
+}
